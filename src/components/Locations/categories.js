88fff3f2d7592fdc19b0ab9ce73a55dba71eb4d0
Daniel Olson <emaildano@gmail.com>
2020-04-11 15:29:50 -0400

    updated cat list

--- src/components/Locations/categories.js
+++ src/components/Locations/categories.js
@@ -4,10 +4,14 @@ import { Badge } from "reactstrap"
 const renderCategoryNodes = (nodes, title) => (
   <div>
     {console.log(nodes)}
-    {nodes.map(term => (
-      <Badge key={term.id} color="primary" pill>
-        {term.name}
-      </Badge>
+    {nodes.map((term, i) => (
+      <Badge
+        key={term.id}
+        color="primary"
+        pill
+        className={i !== 0 ? `ml-2` : null}
+        dangerouslySetInnerHTML={{ __html: term.name }}
+      />
     ))}
   </div>
 )
