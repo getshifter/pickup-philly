03cb9fba7a71a060d682513700a66a16c1756ba5
Jason <jmcclintok07@gmail.com>
2020-04-12 14:45:45 -0400

    updated modal footer spacing

--- src/components/Header/Modal.js
+++ src/components/Header/Modal.js
@@ -18,11 +18,11 @@ const InfoModal = props => {
       </NavLink>
       <Modal isOpen={modal} toggle={toggle} className={className}>
         <ModalHeader toggle={toggle}><img src={logo} alt="logo" className="mt-4 ml-4"/></ModalHeader>
-        <ModalBody className="display-2 px-sm-5 px-xs-0 text-light">
+        <ModalBody className="display-2 px-sm-5 px-xs-0 pb-5 text-light">
         Using the power of open source, let’s create a business directory tool for the Philadelphia community listing all businesses affected by the coronavirus outbreak reaching their customers with how to shop, modified hours of operation, and more.
 
         </ModalBody>
-        <ModalFooter className="mb-4 justify-content-between display-3">
+        <ModalFooter className="pt-5 mb-4 justify-content-between display-3">
           <Col>
           <a className="text-white" href="/">Contact Us <ArrowRight size="2rem" /></a>
           </Col>
