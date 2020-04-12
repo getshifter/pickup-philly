45ff5764ef0dea886d1b502bd25cf4ad6121684d
Jason <jmcclintok07@gmail.com>
2020-04-12 13:46:25 -0400

    added links to footer items

--- src/components/Header/Modal.js
+++ src/components/Header/Modal.js
@@ -17,16 +17,14 @@ const InfoModal = props => {
        {buttonLabel}
       </NavLink>
       <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}><img src={logo} alt="logo" /></ModalHeader>
+        <ModalHeader toggle={toggle}><img src={logo} alt="logo" className="mt-4 ml-4"/></ModalHeader>
         <ModalBody className="display-2 px-5 text-light">
         Using the power of open source, let’s create a business directory tool for the Philadelphia community listing all businesses affected by the coronavirus outbreak reaching their customers with how to shop, modified hours of operation, and more.
 
         </ModalBody>
-        <ModalFooter className="mb-4">
-          <Row className= "d-inline justify-content-between display-2 text-white">
-          Contact Us
-         <Twitter size="2rem"/>
-          </Row>
+        <ModalFooter className="mb-4 d-inline justify-content-between display-2">
+          <a className="text-white" href="/">Contact Us</a>
+          <a className="text-white" href="twitter.com"><Twitter size="2rem"/></a>
         </ModalFooter>
       </Modal>
     </div>
