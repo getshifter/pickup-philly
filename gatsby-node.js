f9f76503524a8bf171a5f0d75135c47556610f6e
Daniel Olson <emaildano@gmail.com>
2020-04-14 18:34:32 -0400

    Updated map webpack

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,3 +11,33 @@
 //   await createCategories({ actions, graphql })
 //   await createTags({ actions, graphql })
 // }
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  const config = getConfig()
+
+  let newConfig = {
+    ...config,
+    module: {
+      ...config.module,
+      noParse: /(mapbox-gl)\.js$/,
+    },
+  }
+
+  if (stage === "build-html") {
+    newConfig = {
+      ...newConfig,
+      module: {
+        ...newConfig.module,
+        rules: [
+          ...newConfig.module.rules,
+          {
+            test: /(mapbox-gl)\.js$/,
+            loader: "null-loader",
+          },
+        ],
+      },
+    }
+  }
+
+  actions.replaceWebpackConfig(newConfig)
+}
