4f8a271e9718eb4627f9f8525b2ef1c07cb315f3
Daniel Olson <emaildano@gmail.com>
2020-04-14 23:32:27 -0400

    updated logo size

--- src/components/Header/Modal.js
+++ src/components/Header/Modal.js
@@ -15,7 +15,7 @@ const InfoModal = props => {
     query {
       logo: file(relativePath: { eq: "pickpuphilly-logo-light.png" }) {
         childImageSharp {
-          fixed {
+          fixed(width: 175) {
             ...GatsbyImageSharpFixed
           }
         }
