4ec322ea9844a49690fa4b93b33b795e32b8d322
Daniel Olson <emaildano@gmail.com>
2020-04-14 20:09:34 -0400

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,12 @@ const activeEnv =
 const config = {
   wordPressUrl:
     activeEnv === "production"
-      ? `https://9e99c91f-7ecf-4aa1-b77a-d18005cf4139.app.getshifter.io:54100/`
-      : "https://9e99c91f-7ecf-4aa1-b77a-d18005cf4139.app.getshifter.io:54100/",
+      ? `REPLACE_SHIFTER_URL/`
+      : "https://9e99c91f-7ecf-4aa1-b77a-d18005cf4139.app.getshifter.io:50003/",
   wordPressGraphQlUrl:
     activeEnv === "production"
-      ? `https://9e99c91f-7ecf-4aa1-b77a-d18005cf4139.app.getshifter.io:54100/graphql/`
-      : "https://9e99c91f-7ecf-4aa1-b77a-d18005cf4139.app.getshifter.io:54100/graphql/",
+      ? `REPLACE_SHIFTER_URL/graphql/`
+      : "https://9e99c91f-7ecf-4aa1-b77a-d18005cf4139.app.getshifter.io:50003/graphql/",
 }
 
 module.exports = { config }
