6d6c0481fd7da948798c53b3d0a8bbf9d728af4e
Daniel Olson <emaildano@gmail.com>
2020-04-17 13:03:11 -0400

    added GATSBY_ALGOLIA_INDEX_NAME

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,7 +104,7 @@ module.exports = {
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        indexName: `locations`,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
         queries,
         chunkSize: 10000, // default: 1000
       },
