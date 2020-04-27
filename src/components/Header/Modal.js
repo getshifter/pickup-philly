e47b63bf5a3e49739976f39556207e89507f548a
Jason <jmcclintok07@gmail.com>
2020-04-27 15:45:39 -0400

    logo spacing and padding adjustment

--- src/components/Header/Modal.js
+++ src/components/Header/Modal.js
@@ -94,27 +94,26 @@ const InfoModal = props => {
           </div>
   
         </ModalFooter>
-        <div className="display-4 border-top border-light w-100 p-4 d-flex justify-content-between align-items-start">
+        <div className="display-4 border-top border-light w-100 p-5 d-flex justify-content-between align-items-start">
           <div className="small text-light ">Proudly Sponsored by</div>
           <div>
           <a href="https://phillylovesbeer.org/">
            <Img
             fixed={data.plb.childImageSharp.fixed}
-            className="ml-4 align-text-middle"
             alt=""
           />
           </a>
           <a href="https://www.phillytapfinder.com/">
           <Img
             fixed={data.ptf.childImageSharp.fixed}
-            className="ml-4 align-text-bottom"
+            className="ml-5"
             alt=""
           />
           </a>
           <a href="https://www.getshifter.io/">
            <Img
             fixed={data.shifter.childImageSharp.fixed}
-            className="ml-5 align-text-middle"
+            className="ml-5"
             alt=""
           />
           </a>
